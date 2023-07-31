5eef7b44b88eeccf02bd8754a9ee0a86574b61ae
Pat Broders <patrick,broders73@ed.cna.nl.ca>
2023-07-31 12:46:57 -0230

    Added custom bootstrap options for search bar in nav and footer

--- application.py
+++ application.py
@@ -1,6 +1,6 @@
 import os
-from flask import Flask, redirect, url_for, request, render_template
-from forms import RecipeForm
+from flask import Flask, redirect, url_for, render_template, request
+from forms import RecipeForm, SearchForm
 import pandas as pd
 from werkzeug.utils import secure_filename
 
@@ -11,29 +11,55 @@ app.config['SUBMITTED_DATA'] = os.path.join("static", "data_dir", "")
 app.config['SUBMITTED_IMG'] = os.path.join("static", "image_dir", "")
 
 
+
+
 @app.route('/')
 def hello_world():
     return render_template("index.html")
 
 
-@app.route('/add_recipe', methods=["Post", "Get"])
+@app.route('/add_recipe', methods=["POST", "GET"])
 def add_recipe():
+    """
+    A function that uses a pre-built form that allows a user to add a recipe to the database, including file handling.
+    :return:
+    """
+    df = pd.read_csv("static/data_dir/recipes.csv", dtype={"name": str})
+    df = df.set_index("name")
     form = RecipeForm()
     if form.validate_on_submit():
         recipe_name = form.recipe_name.data
-        picture_filename = (recipe_name.lower().replace(" ", "_") + "." +
-                            secure_filename(form.recipe_picture.data.filename).split(".")[-1])
-        form.recipe_picture.data.save(os.path.join(app.config['SUBMITTED_IMG'] + picture_filename))
-        df = pd.DataFrame([{'name': recipe_name, 'picture':picture_filename}])
-        df.to_csv(os.path.join(app.config["SUBMITTED_DATA"] + recipe_name.lower().replace(" ", "_") + ".csv"))
-        return redirect(url_for("hello_world"))
+        if recipe_name not in df:
+            recipe_ingredients = form.recipe_ingredients.data
+            recipe_instructions = form.recipe_instructions.data
+            recipe_serving_size = form.recipe_serving_size.data
+            picture_filename = (recipe_name.lower().replace(" ", "_") + "." +
+                                secure_filename(form.recipe_picture.data.filename).split(".")[-1])
+            form.recipe_picture.data.save(os.path.join(app.config["SUBMITTED_IMG"] + picture_filename))
+            df.loc[recipe_name] = [recipe_ingredients, recipe_instructions, recipe_serving_size, picture_filename]
+            df.to_csv("static/data_dir/recipes.csv")
+            return redirect(url_for("hello_world"))
+        else:
+            return render_template("add_recipe.html", form=form)
+
     else:
         return render_template("add_recipe.html", form=form)
 
 
-@app.route("/search_recipe", methods=["Post", "Get"])
+@app.route("/search_recipe", methods=["POST", "GET"])
 def search_recipe():
-    return render_template("search_recipe.html")
+    search = SearchForm(request.form)
+    if request.method == "POST":
+        return display_recipe(search)
+
+    return render_template("search_recipe.html", form=search)
+
+
+@app.route("/display_recipe")
+def display_recipe(search):
+    df = pd.read_csv("static/data_dir/recipes.csv")
+    recipe = df.iloc[0]
+    return render_template("view_recipe.html", recipe=recipe)
 
 
 if __name__ == "__main__":
--- forms.py
+++ forms.py
@@ -1,10 +1,19 @@
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileRequired, FileField
-from wtforms.fields import StringField, TextAreaField
-from wtforms.fields.html5 import DateField, EmailField, TelField
-from wtforms.validators import DataRequired, Length
+from wtforms import SubmitField
+from wtforms.fields import StringField, TextAreaField, IntegerField
+from wtforms.validators import DataRequired
 
 
 class RecipeForm(FlaskForm):
     recipe_name = StringField("Recipe Name:", validators=[DataRequired()])
+    recipe_ingredients = TextAreaField("Recipe Ingredient List:", validators=[DataRequired()])
+    recipe_instructions = TextAreaField("Recipe Instructions:", validators=[DataRequired()])
+    recipe_serving_size = IntegerField("Recipe Serving Size:", validators=[DataRequired()])
     recipe_picture = FileField("Recipe Field: ", validators=[FileRequired()])
+    submit = SubmitField("Submit")
+
+
+class SearchForm(FlaskForm):
+    search = StringField("Search", validators=[DataRequired()])
+    submit = SubmitField("Submit")
